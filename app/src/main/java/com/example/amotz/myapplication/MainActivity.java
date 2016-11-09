4eea2ab5b638d1ad89dd8a29a6e840a0d6b998f1
Amotz <amotzte@gmail.com>
2016-11-09 08:10:12 +0200

    Adding one more comment

--- app/src/main/java/com/example/amotz/myapplication/MainActivity.java
+++ app/src/main/java/com/example/amotz/myapplication/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // My changes here
+	// My changes for ex3 
     }
 }
